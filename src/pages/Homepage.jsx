aa6c140f53a66a3db7548a35d88d4049fcfc0503
André Torres <150715327+andtors@users.noreply.github.com>
2025-07-10 13:25:16 -0300

    Cards homepage ajustado

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,8 @@
 import "./Homepage.css"
-import CardComponent from "../components/CardComponent"
 
 import Card from '@mui/material/Card';
+
+import CardComponent from "../components/CardComponent";
 
 import useDevApi from "../hooks/useDevApi"
 import { useEffect, useState } from "react";
@@ -38,14 +39,14 @@ const Homepage = () => {
         <h2>Confira abaixo alguma de nossas viagens!</h2>
 
         <div id="home-components">
-            {articles.map((article) => (
-                <CardComponent width={300} height={300} key={article.id}  title={article.title} description={article.description} image={article.social_image} url={article.url} date={article.published_at}/>
-            ))}
-            <Card sx={{ maxWidth: 300, backgroundColor: "#00558a", marginBottom: 5}} >
+            <Card sx={{ height:200, width: 800, backgroundColor: "#00558a", marginBottom: 5}} >
                 <div id="home-card-blog">
                     <h3 onClick={() => navigate("/blog")}>Acesse nosso blog clicando aqui!</h3>
                 </div>
             </Card>
+            {articles.map((article) => (
+                  <CardComponent width={800} key={article.id} height="300" title={article.title} description={article.description} image={article.social_image} url={article.url} date={article.published_at}/>
+            ))}
         </div>
     </div>
   )
